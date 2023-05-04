f35c4a427702086eae08018fba52520060a17c0a
RobertoNesta <robertonesta1993@gmail.com>
2023-05-04 14:53:09 +0200

    error

--- snack 2/access.php
+++ snack 2/access.php
@@ -4,11 +4,24 @@ $name = $_GET["name"];
 $mail = $_GET["mail"];;
 $age = $_GET["age"];;
 
-if(strlen($name) > 3 && str_contains($mail, "@, ." )  && is_numeric($age)) {
 
-    access confirmed
-};
-else(){
-    access denied
-};
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php if(strlen($name) > 3 && str_contains($mail, "@") && str_contains($mail, ".")  && is_numeric($age)) : ?>
+<p>access confirmed</p>
+<?php else : ?>
+<p>access denied</p>
+<?php endif; ?>
+
+</body>
+</html>
--- snack 2/index.php
+++ snack 2/index.php
@@ -14,7 +14,7 @@
 <body>
 <!--Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”-->
 
-<form action="access.php" method="GET">
+<form action="access.php" method="get">
     <div>
         <label for="name">name</label>
         <input type="text" name="name" id="name" class="form-control" placeholder="name" aria-describedby="suffixId">
