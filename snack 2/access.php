e3809268ebbd3ed04c7a6f880e2840690d7937db
RobertoNesta <robertonesta1993@gmail.com>
2023-05-04 13:03:02 +0200

    exercise uncomplete

--- snack 2/access.php
+++ snack 2/access.php
@@ -1,3 +1,14 @@
 <?php
 
+$name = $_GET["name"];
+$mail = $_GET["mail"];;
+$age = $_GET["age"];;
+
+if(strlen($name) > 3 && $mail str_contains  && $age == 30) {
+
+    access confirmed
+};
+else(){
+    access denied
+};
 ?>
